1143659bf63b4c33ef2e9e944f3652b80948c621
konstantysz <kostekszumigaj@gmail.com>
2025-09-13 16:27:06 +0200

    Working hello world window

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,107 +1,180 @@
-#include <iostream>
-
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <imgui_internal.h>
+#include <iostream>
+#include <spdlog/spdlog.h>
+#include <vector>
 
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-
-template<typename T>
-concept Number = std::integral<T> || std::floating_point<T>;
-
-template<Number T>
-T add(T a, T b) { return a + b; }
-
-int main() {
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW\n";
-        return -1;
+GLFWwindow *CreateMainWindow(int width, int height, const char *title)
+{
+    if (!glfwInit())
+    {
+        spdlog::error("Failed to initialize GLFW");
+        return nullptr;
     }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "VisionCraft", nullptr, nullptr);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window\n";
+    GLFWwindow *window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+    if (!window)
+    {
+        spdlog::error("Failed to create GLFW window");
         glfwTerminate();
-        return -1;
+        return nullptr;
     }
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
+    return window;
+}
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "Failed to initialize GLAD\n";
-        return -1;
+bool InitOpenGLLoader()
+{
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        spdlog::error("Failed to initialize GLAD");
+        return false;
     }
+    return true;
+}
 
-    // Setup ImGui
+void InitImGui(GLFWwindow *window)
+{
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
     ImGui::StyleColorsDark();
     ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 330");
+    ImGui_ImplOpenGL3_Init("#version 450");
+}
 
-    bool showInspector = true;
-    bool showCanvas = true;
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
-        // Menu bar
-        if (ImGui::BeginMainMenuBar()) {
-            if (ImGui::BeginMenu("File")) {
-                if (ImGui::MenuItem("Exit")) glfwSetWindowShouldClose(window, 1);
-                ImGui::EndMenu();
-            }
-            if (ImGui::BeginMenu("Help")) {
-                ImGui::MenuItem("About VisionCraft");
-                ImGui::EndMenu();
-            }
-            ImGui::EndMainMenuBar();
-        }
-
-        // Canvas panel
-        if (showCanvas) {
-            ImGui::Begin("Canvas", &showCanvas);
-            ImGui::Text("This will be the node editor in the future!");
-            ImGui::End();
-        }
-
-        // Inspector panel
-        if (showInspector) {
-            ImGui::Begin("Inspector", &showInspector);
-            ImGui::Text("Object properties panel");
-            ImGui::Text("2 + 3 = %d", add(2, 3)); // Example using concept
-            ImGui::End();
-        }
-
-        // Rendering
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        glfwSwapBuffers(window);
-    }
-
-    // Cleanup
+void ShutdownImGui()
+{
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
+}
+
+void ShutdownGLFW(GLFWwindow *window)
+{
     glfwDestroyWindow(window);
     glfwTerminate();
+}
+
+void RenderMainDockspace()
+{
+    static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
+    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+    const ImGuiViewport *viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(viewport->WorkPos);
+    ImGui::SetNextWindowSize(viewport->WorkSize);
+    ImGui::SetNextWindowViewport(viewport->ID);
+    window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize
+                    | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+    ImGui::Begin("MainDockspaceWindow", nullptr, window_flags);
+    ImGui::PopStyleVar(2);
+    ImGuiID dockspace_id = ImGui::GetID("MainDockspace");
+    ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+    static bool dock_initialized = false;
+    if (!dock_initialized)
+    {
+        dock_initialized = true;
+        ImGui::DockBuilderRemoveNode(dockspace_id);
+        ImGui::DockBuilderAddNode(dockspace_id, ImGuiDockNodeFlags_DockSpace);
+        ImGuiID left_id, right_id;
+        ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Left, 0.2f, &left_id, &right_id);
+        ImGui::DockBuilderDockWindow("Inspector", left_id);
+        ImGui::DockBuilderDockWindow("Canvas", right_id);
+        ImGui::DockBuilderFinish(dockspace_id);
+    }
+    if (ImGui::BeginMenuBar())
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            if (ImGui::MenuItem("Run"))
+            {
+                spdlog::info("Run button clicked!");
+            }
+            ImGui::EndMenu();
+        }
+        ImGui::EndMenuBar();
+    }
+    ImGui::End();
+}
+
+void RenderCanvasPanel()
+{
+    ImGui::Begin("Canvas");
+    ImGui::Text("Canvas will go here...");
+    ImGui::End();
+}
+
+void RenderInspectorPanel()
+{
+    ImGui::SetNextWindowSizeConstraints(ImVec2(200, 0), ImVec2(FLT_MAX, FLT_MAX));
+    ImGui::SetNextWindowSize(ImVec2(200, 0), ImGuiCond_Once);
+    ImGui::Begin("Inspector");
+    ImGui::Text("Inspector panel...");
+    ImGui::End();
+}
+
+void RenderFrame(GLFWwindow *window)
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+    RenderMainDockspace();
+    RenderCanvasPanel();
+    RenderInspectorPanel();
+    ImGui::Render();
+    int display_w, display_h;
+    glfwGetFramebufferSize(window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    ImGuiIO &io = ImGui::GetIO();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        GLFWwindow *backup_current_context = glfwGetCurrentContext();
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+        glfwMakeContextCurrent(backup_current_context);
+    }
+    glfwSwapBuffers(window);
+}
+
+int main()
+{
+    GLFWwindow *window = CreateMainWindow(1280, 720, "Vision Craft");
+    if (!window)
+    {
+        return -1;
+    }
+
+    if (!InitOpenGLLoader())
+    {
+        ShutdownGLFW(window);
+        return -1;
+    }
+
+    InitImGui(window);
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        RenderFrame(window);
+    }
+
+    ShutdownImGui();
+    ShutdownGLFW(window);
+    
     return 0;
 }

98883903df54a844657cf85bd438c66db59da62a
konstantysz <kostekszumigaj@gmail.com>
2025-11-21 20:53:48 +0100

    Initialize node factory on application startup

--- src/App/VisionCraftGUI.cpp
+++ src/App/VisionCraftGUI.cpp
@@ -1,7 +1,11 @@
 #include "App/VisionCraftApplication.h"
+#include "Vision/Factory/NodeFactory.h"
 
 int main()
 {
+    // Register all node types before creating application
+    VisionCraft::Vision::NodeFactory::RegisterAllNodes();
+
     Kappa::ApplicationSpecification appSpec;
     appSpec.name = "VisionCraft";
     appSpec.windowSpecification.title = "VisionCraft - Computer Vision Node Editor";
